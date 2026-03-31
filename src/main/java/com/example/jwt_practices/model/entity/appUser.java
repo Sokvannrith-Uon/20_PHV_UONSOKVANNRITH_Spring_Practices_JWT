45b5bc4b39b3e9029e2434df47b9a5bf363c6242
Sokvannrith.Uon <uonsokvannrith@gmail.com>
2026-03-31 22:47:36 +0700

    model

--- src/main/java/com/example/jwt_practices/model/entity/appUser.java
+++ src/main/java/com/example/jwt_practices/model/entity/appUser.java
@@ -5,26 +5,37 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class appUser implements UserDetails {
+    private Long userId;
+    private String fullName;
+    private String email;
+    private String password;
+    private List<String> roles;
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        for(String role : roles){
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+        return authorities;
     }
 
     @Override
     public @Nullable String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return email;
     }
 }
